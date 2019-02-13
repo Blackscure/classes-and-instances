dee4d5fb0a1e88e6b5777dbd3548f9bf05bbfa20
Blackscure <Wekesabuyahi@gmail.com>
2019-02-13 09:47:46 +0300

    class file

---
+++ class.py
@@ -0,0 +1,18 @@
+class Employee:
+
+    def __init__(self,first,last,pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@company.com'
+
+emp_1 = Employee('Hezzy','Buyahi',5000)
+emp_2 = Employee('zzy','yahi',8000)
+
+#print(emp_1)
+#print(emp_2)
+
+print(emp_1.email)
+print(emp_2.email)
+
+ 
